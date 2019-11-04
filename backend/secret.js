92a826809948e2d1ac79bcef6bee69d9613e21e5
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-04 05:17:04 -0300

    Mongo connectado com o front

--- backend/secret.js
+++
@@ -1,7 +0,0 @@
-const secrets = {
-  dbUri: "mongodb+srv://ergastAdmin:2foi3vai7zdp@cluster0-dkrcl.mongodb.net/test?retryWrites=true&w=majority"
-};
-
-const getSecret = key => secrets[key];
-
-module.exports = getSecret;
--- client/src/ComponenteMain/App.js
+++ client/src/ComponenteMain/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from "axios";
 import './EstiloMain/App.css';
 import './EstiloMain/bg.png';
 
@@ -6,12 +7,181 @@ import './EstiloMain/bg.png';
 import BarraHorizontal from '../Componentes/BarraHorizontal';
 import BarraVertical from '../Componentes/BarraVertical';
 
-const API_KEY = "81101292e91a941f627ea27ec02cf4bd";
-
-class App extends React.Component{
+class App extends Component{
 
   //Pegando as alterações nos dados de busca por busca, armazenamento em estados(states), inicialmente "undefined"
   state = {
+    data: [],
+    id: 0,
+    message: null,
+    intervalIsSet: false,
+    idToDelete: null,
+    idToUpdate: null,
+    objectToUpdate: null
+  }
+
+  componentDidMount() {
+    this.getDataFromDb();
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getDataFromDb, 1000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+
+  getDataFromDb = () => {
+    fetch("http://localhost:3001/api/getData")
+      .then(data => data.json())
+      .then(res => this.setState({ data: res.data }));
+  };
+
+  putDataToDB = message => {
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      ++idToBeAdded;
+    }
+
+    axios.post("http://localhost:3001/api/putData", { 
+      id: idToBeAdded,
+      message: message
+    });
+  };
+
+  deleteFromDB = idTodelete => {
+    let objIdToDelete = null;
+    this.state.data.forEach(dat => {
+      if (dat.id === idTodelete) {
+        objIdToDelete = dat._id;
+      }
+    });
+
+    axios.delete("http://localhost:3001/api/deleteData", {
+      data: {
+        id: objIdToDelete
+      }
+    });
+  };
+
+  updateDB = (idToUpdate, updateToApply) => {
+    let objIdToUpdate = null;
+    this.state.data.forEach(dat => {
+      if (dat.id === idToUpdate) {
+        objIdToUpdate = dat._id;
+      }
+    });
+
+    axios.post("http://localhost:3001/api/updateData", {
+      id: objIdToUpdate,
+      update: { message: updateToApply }
+    });
+  };
+
+  render(){
+    const { data } = this.state;
+    return (
+
+      <div className="row">
+
+        <div className="col-2 p-0">
+          <BarraVertical />
+        </div>
+
+        <div className="col-10 p-0">
+          <div className="bar-horizontal">
+            <BarraHorizontal/>
+          </div>
+          <div className="wrapper">
+              <div className="row w-75 mx-auto">
+                <div className="col-12"> 
+                  <ul>
+                    {data.length <= 0 ? "NO DB ENTRIES YET" : data.map(dat => (
+                      <li style={{ padding: "10px" }} key={dat}>
+                        <span style={{ color: "gray" }}> id: </span> {dat.id} <br />
+                        <span style={{ color: "gray" }}> data: </span>
+                        {dat.message}
+                      </li>
+                    ))}
+                  </ul>
+                  <div style={{ padding: "10px" }}>
+                    <input
+                      type="text"
+                      onChange={e => this.setState({ message: e.target.value })}
+                      placeholder="add something in the database"
+                      style={{ width: "200px" }}
+                    />
+                    <button onClick={() => this.putDataToDB(this.state.message)}>
+                      ADD
+                    </button>
+                  </div>
+                  <div style={{ padding: "10px" }}>
+                    <input
+                      type="text"
+                      style={{ width: "200px" }}
+                      onChange={e => this.setState({ idToDelete: e.target.value })}
+                      placeholder="put id of item to delete here"
+                    />
+                    <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
+                      DELETE
+                    </button>
+                  </div>
+                  <div style={{ padding: "10px" }}>
+                    <input
+                      type="text"
+                      style={{ width: "200px" }}
+                      onChange={e => this.setState({ idToUpdate: e.target.value })}
+                      placeholder="id of item to update here"
+                    />
+                    <input
+                      type="text"
+                      style={{ width: "200px" }}
+                      onChange={e => this.setState({ updateToApply: e.target.value })}
+                      placeholder="put new value of the item here"
+                    />
+                    <button
+                      onClick={() =>
+                        this.updateDB(this.state.idToUpdate, this.state.updateToApply)
+                      }
+                    >
+                      UPDATE
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    );
+  }
+
+}
+
+export default App;
+
+
+/* ----------------------------------------------- COMPONENTES QUE AINDA PODEM SER APROVEITADOS
+
+<Formulario getData={this.getData}/>
+
+<ClimaInfo
+  temperatura={this.state.temperatura}
+  cidade={this.state.cidade}
+  pais={this.state.pais}
+  umidade={this.state.umidade}
+  latitude={this.state.latitude}
+  longitude={this.state.longitude}
+  error={this.state.error}
+/>
+
+<Listagens/>
+
     temperatura: undefined,
     cidade: undefined,
     pais: undefined,
@@ -19,9 +189,10 @@ class App extends React.Component{
     latitude: undefined,
     longitude: undefined,
     error: undefined
-  }
 
-  getData = async (e) => {
+----------------------------------------> Parte da main abaixo de state:
+
+ getData = async (e) => {
     
     e.preventDefault();//Previne que a requisição disparada não resete pelo reload do React
     
@@ -57,56 +228,6 @@ class App extends React.Component{
 
   }
 
-  render(){
-    return (
-
-      <div className="row">
-
-        <div className="col-2 p-0">
-          <BarraVertical />
-        </div>
-
-        <div className="col-10 p-0">
-          <div className="bar-horizontal">
-            <BarraHorizontal/>
-          </div>
-          <div className="wrapper">
-            <div className="row w-75 mx-auto">
-                <div className="col-12">
-                  
-                </div>
-                <div className="col-4">
-                  
-                </div>
-                
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-    );
-  }
-
-}
-
-export default App;
-
-
-/* ----------------------------------------------- COMPONENTES QUE AINDA PODEM SER APROVEITADOS
-
-<Formulario getData={this.getData}/>
-
-<ClimaInfo
-  temperatura={this.state.temperatura}
-  cidade={this.state.cidade}
-  pais={this.state.pais}
-  umidade={this.state.umidade}
-  latitude={this.state.latitude}
-  longitude={this.state.longitude}
-  error={this.state.error}
-/>
-
-<Listagens/>
+  const API_KEY = "81101292e91a941f627ea27ec02cf4bd";
 
 */
