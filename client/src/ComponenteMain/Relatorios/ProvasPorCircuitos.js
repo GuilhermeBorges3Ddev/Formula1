b9b9bdd288e78546301ebe2d8fd1a13390644db8
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-05 04:10:39 -0300

    Coleções de relatórios adicionadas na interface

---
+++ client/src/ComponenteMain/Relatorios/ProvasPorCircuitos.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+//import axios from "axios";
+import './EstiloMain/App.css';
+import './EstiloMain/bg.png';
+
+import BarraHorizontal from '../../Componentes/BarraHorizontal';
+import BarraVertical from '../../Componentes/BarraVertical';
+
+class ProvasPorCircuitos extends Component{
+
+  state = {
+    data: [],
+    id: 0,
+    message: null,
+    intervalIsSet: false,
+    idToDelete: null,
+    idToUpdate: null,
+    objectToUpdate: null
+  }
+
+  componentDidMount() {
+    this.getDataFromDb();
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getDataFromDb, 1000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+
+  getDataFromDb = () => {
+    fetch("http://localhost:3001/api/getData")
+      .then(data => data.json())
+      .then(res => this.setState({ data: res.data }));
+  };
+  
+  render(){
+    
+    return (
+
+      <div className="row">
+
+        <div className="col-2 p-0">
+          <BarraVertical />
+        </div>
+
+        <div className="col-10 p-0">
+          <div className="bar-horizontal">
+            <BarraHorizontal/>
+          </div>
+          <div className="wrapper">
+
+              <div className="row w-75 ml-5 justify-content-between align-items-start">
+                 Provas por circuitos
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+    );
+  }
+
+}
+
+export default ProvasPorCircuitos;
